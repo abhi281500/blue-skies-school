49578d1b6e64213a8ab2ac3efaa7d08b6dd88e92
abhi281500 <abhi281500@gmail.com>
2026-02-15 12:12:45 +0530

    SEO metadata and Google verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,11 +6,14 @@ export const metadata = {
   title: "Blue Skies School | CBSE School in East Champaran",
   description:
     "Blue Skies School is a CBSE curriculum school in East Champaran offering quality education from Nursery to Class VIII. Admissions Open 2026–27.",
+    verification: {
+    google: "WijdLBAPpZhg8MHt-IiJYZQD91QcaQpm1l_TjF9IM8c",
+  },
   keywords: [
     "Blue Skies School",
     "CBSE School in East Champaran",
-    "Best School in East Champaran",
-    "Nursery School East Champaran",
+    "Best School in Motihari",
+    "School Near  Pipra Purani Chown",
     "Primary School East Champaran",
   ],
 };
