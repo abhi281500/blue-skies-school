6421496377b07b946342decbee1328baf1ea8a9b
abhi281500 <abhi281500@gmail.com>
2026-02-15 14:06:25 +0530

    add robots.txt for SEO

---
+++ app/robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://blue-skies-school.vercel.app/sitemap.xml",
+  };
+}
